af0aa2981e4292eca69a2dca3d7e1dbf86969cd0
Anton Ekberg <anton.ekberg@skf.com>
2021-01-19 12:23:58 +0100

    NodeMetadata is map[string]*string not map[string]string

--- scripts/patch-x-nullable.sh
+++ scripts/patch-x-nullable.sh
@@ -4,6 +4,7 @@ OUTPUT=$(
   cat "$1" \
   | jq '.definitions.Node["x-nullable"] = false' \
   | jq '.definitions.Origin["x-nullable"] = false' \
+  | jq '.definitions.NodeMetaData.additionalProperties["x-nullable"] = true' \
 )
 
 [[ $? == 0 ]] && echo "${OUTPUT}" >| $1
